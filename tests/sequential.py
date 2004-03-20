98ddfdefc7e8dbca974ccee50d1b45e300ed0ac8
jonas <jonas@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2004-03-20 19:24:31 +0000

    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/hardware/acqboard@518 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/sequential.py
+++ tests/sequential.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from struct import *
-f=open('/home/jonas/raw.nodomain.dat', 'rb')
+f=open('/home/jonas/data.raw.dat', 'rb')
 
 sample = f.read(32)
 pos = 0
@@ -12,14 +12,17 @@ while sample:
     line = line + 1
     sample = f.read(32)
     if len(sample) < 32 :
-        line
+        print line
     result = unpack("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", sample);
 
-    for i in range(24):
+    #for i in range(24):
         
-        if result[i+1] != (pos+1) and (pos != 238):
-            #print "error at ",  pos+1,  result[i+1]
-            #print result
-            print line
-        pos = result[i+1]
-        
+    #    if result[i+1] != (pos+1) and (pos != 238):
+    #        print "error at ",  pos+1,  result[i+1]
+    #        print result
+    #        print line
+    #    pos = result[i+1]
+    #simple test
+    if result[0] != 188:
+        print result
+        print line

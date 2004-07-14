66296aa6610011b6ecc450e5c316da3c000901f0
/C=US/ST=Massachusetts/L=Cambridge/O=Wilson Lab, PCLM, MIT/OU=The Soma Project/CN=jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=Wilson Lab, PCLM, MIT/OU=The Soma Project/CN=jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2004-07-14 16:58:21 +0000

    Ready to implement new data setup
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/hardware/acqboard@685 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/analysis/showdata.py
+++ tests/analysis/showdata.py
@@ -4,56 +4,45 @@ from scipy import *
 from matplotlib.matlab import *
 
 import readacq
+from tables import *
 
-def plotmain():
-    raw = readacq.RawFile('/home/jonas/test.dat')
 
-    x = raw.read(2**16)/32768.0
+def plotfft(source, location = None, entry = None):
 
-    plot(x[0:1000])
-    show()
+    
+    if isinstance( source, str):
+        raw = readacq.RawFile(source)
+
+        x = raw.read(2**16)/32768.0
+    else :
+        # HD5f data
+        x = source['data'][0:2**16]
+
     Fs = 256000
     phix = fft(x)
-
-    print "The length is", len(phix)
 
     normrealphix = abs(phix[0:(len(phix)/2)])**2 / max(abs(phix)**2)
 
     w = r_[0:2**15]/float(2**15) * (Fs/2)
 
-    print len(normrealphix), len(w)
-    plot(w,10*log10 (normrealphix), 'r')
+
+    plot(w,10*log10(normrealphix), 'r')
     
     
-    axis([ 0, 10000, -100, 1])
+    axis([ 0, 32000, -100, 1])
     xlabel('Frequency (Hz)')
     ylabel('Power (dB)')
     grid(1)
     
     show()
-    
+
+
+import sys
 def main():
     
-    raw = readacq.RawFile('test.247.16.dat')
-
-    x = raw.read(2**16)/32768.0
-
-    fs = 256000 
-    fftlen = 2**12
-    n = fftlen * 2**4
-    
-    phi = periodogram.periodogram(x, windows.hanning(n), fftlen)
-    print mean(phi)
-    
-    plot(r_[0:fs:(float(fs)/fftlen)] ,20*log10 (phi))
-    
-    grid(1)
-    xlabel('Frequency (Hz)')
-    ylabel('Power (dB)') 
-    #axis([0, 128000, -100, 10])
-    show()
-    
+    h5file = openFile("../results/test.h5", "r")
+    plotfft(h5file.root.raw.sineruns[0])
 
 
 if __name__ == "__main__":
-    plotmain()
+    main()

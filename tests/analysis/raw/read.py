a67d603924ffb8a4672d58dbed9fd9e455e8cd77
jonas@soma.mit.edu <jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2005-02-01 16:10:39 +0000

    we're now using raw. as a module; adjusting naming and code to reflect that
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/hardware/acqboard@884 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

---
+++ tests/analysis/raw/read.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+from numarray import *
+import socket
+from os import unlink
+from struct import *
+
+"""
+
+Code to read in n samples of raw data via the protointerface domain socket system
+"""
+
+def read(nsamples):
+
+
+
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    
+    
+    s.connect("/tmp/acqboard.out")
+    
+    
+    
+    nsamp = 0; 
+    
+    resultstr = ""
+    sample = s.recv(512)
+    
+    
+    while (nsamp < (2*nsamples*24.0/16.0 + 200)):
+        tmpstr =s.recv(1024)
+        nsamp += len(tmpstr)
+        resultstr += tmpstr
+        
+    offset = 18
+    datastr = resultstr[offset:]
+
+        
+
+    # now, we format
+    data = zeros(nsamples, Int16)
+    
+    pos = 0
+    for i in range(len(datastr)/2):
+
+        #print i, len(datastr), len(datastr)/2, pos
+        if i % 12 < 8 :
+            if pos < nsamples:
+                data[pos] = unpack(">h", datastr[(2*i):(2*(i+1))])[0]
+                pos += 1             
+
+    s.close()
+
+    
+    return data

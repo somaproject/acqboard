8275e84860da10dce2156e5865b34fb7d8906e55
jonas@soma.mit.edu <jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2005-02-03 19:02:24 +0000

    * Updated testing to reflect new code to run automated tests   * RMAC docs converted   * PGALoad was rewritten to deal with new shift register design     * Docs converted, updated     * code rewritten
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/hardware/acqboard@885 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- vhdl/PGAload.vhd
+++ vhdl/PGAload.vhd
@@ -16,7 +16,7 @@ entity PGAload is
            SOUT : out std_logic;
            CHAN : in std_logic_vector(3 downto 0);
            GAIN : in std_logic_vector(2 downto 0);
-		 FILTER : in std_logic_vector(1 downto 0); 
+		 FILTER : in std_logic; 
            GSET : in std_logic;
            ISET : in std_logic;
 		 FSET : in std_logic;
@@ -30,18 +30,30 @@ architecture Behavioral of PGAload is
 -- it to serialize out to the shift registers.  The use of LUTs as 
 -- 16x1bit RAMs with async reads makes the design really compact. 
 
-   signal zero, lsout, lsclk, latch, chansel : std_logic := '0';
-   signal bitout : std_logic := '0';
-   signal i, f : std_logic_vector(1 downto 0) := (others => '0');
-   signal g : std_logic_vector(2 downto 0) := (others => '0');
-   signal lbit : std_logic_vector(6 downto 0) := (others => '0');
-   signal osel : std_logic_vector(6 downto 0) := (others => '0');
-   signal addr : std_logic_vector(3 downto 0) := (others => '0');
    signal fwe, iwe, gwe : std_logic := '0';
+   signal g: std_logic_vector(2 downto 0) := (others => '0'); 
+   signal f : std_logic := '0';
+   signal i : std_logic_vector(1 downto 0) := (others => '0'); 
 
-   type states is (none, write_wait, outl, clkl1, clkh1, clkh2, clkl2,
+   signal d: std_logic_vector(5 downto 0) := (others => '0');
+   signal dout : std_logic := '0';
+
+   signal lsout, lsclk, latch : std_logic := '0';
+
+   signal chanL, chanmux : std_logic_vector(3 downto 0)
+   	:= (others => '0');
+
+   signal zero : std_logic := '0';
+
+   signal oaddr : std_logic_vector(5 downto 0) := (others => '0');
+   signal osel : std_logic_vector(6 downto 0) := (others => '0'); 
+
+   signal chansel : std_logic := '0';
+
+
+
+   type states is (none, writew, outl, clkl1, clkh1, clkh2, clkl2,
    			    incsel, latchl, latchh1, latchh2) ;
-			    
    signal cs, ns : states := none;
        
 	component distRAM is
@@ -55,57 +67,125 @@ architecture Behavioral of PGAload is
 begin
 
    -- wire up distributed RAM:
-   gain_RAM_0: distRAM port map (
-   		CLK => CLK,
-		WE => gwe,
-		A => addr,
+
+   ramg0 : distRAM port map (
+   		CLK => clk,
+		WE => gwe, 
+		A => chanmux,
 		DI => g(0),
-		DO => LBIT(6));
+		DO => d(0)); 
 
-   gain_RAM_1: distRAM port map (
-   		CLK => CLK,
-		WE => gwe,
-		A => addr,
+   ramg1 : distRAM port map (
+   		CLK => clk,
+		WE => gwe, 
+		A => chanmux,
 		DI => g(1),
-		DO => LBIT(5));
+		DO => d(1)); 
 
-   gain_RAM_2: distRAM port map (
-   		CLK => CLK,
-		WE => gwe,
-		A => addr,
+   ramg2 : distRAM port map (
+   		CLK => clk,
+		WE => gwe, 
+		A => chanmux,
 		DI => g(2),
-		DO => LBIT(4));
+		DO => d(2)); 
 
-   filter_RAM_0: distRAM port map (
-   		CLK => CLK,
-		WE => fwe,
-		A => addr,
-		DI => f(0),
-		DO => LBIT(3));
+   ramf : distRAM port map (
+   		CLK => clk,
+		WE => fwe, 
+		A => chanmux,
+		DI => f,
+		DO => d(3)); 
 
-   filter_RAM_1: distRAM port map (
-   		CLK => CLK,
-		WE => fwe,
-		A => addr,
-		DI => f(1),
-		DO => LBIT(2));
-
-   input_RAM_0: distRAM port map (
-   		CLK => CLK,
-		WE => iwe,
-		A => addr,
+   rami0 : distRAM port map (
+   		CLK => clk,
+		WE => iwe, 
+		A => chanmux,
 		DI => i(0),
-		DO => LBIT(1));
+		DO => d(4)); 
 
-   input_RAM_1: distRAM port map (
-   		CLK => CLK,
-		WE => iwe,	 
-		A => addr,
+   rami1 : distRAM port map (
+   		CLK => clk,
+		WE => iwe, 
+		A => chanmux,
 		DI => i(1),
-		DO => LBIT(0));
+		DO => d(5)); 
 
 
+   g(0) <= GAIN(0) and not zero; 
+   g(1) <= GAIN(1) and not zero;
+   g(2) <= GAIN(2) and not zero; 
 
+   gwe <= zero or GSET; 
+   
+   f <= FILTER and not zero; 
+   fwe <= FSET or zero; 
+
+   i(0) <= ISEL(0) and not zero; 
+   i(1) <= ISEL(1) and not zero; 
+
+   iwe <= ISET or zero; 
+
+   chanmux <= CHAN when chansel = '0' else osel(6 downto 3); 
+
+   dout <= d(0) when osel(2 downto 0) = "000" else
+			d(1) when osel(2 downto 0) = "001" else
+			d(2) when osel(2 downto 0) = "010" else
+			d(3) when osel(2 downto 0) = "011" else
+			d(4) when osel(2 downto 0) = "100" else
+			d(5);
+
+   lsout <= dout and (not zero); 
+
+
+   OSEL <= "0001011" when oaddr = "000000" else
+   		 "0001000" when oaddr = "000001" else
+		 "0001001" when oaddr = "000010" else
+		 "0001010" when oaddr = "000011" else
+		 "0000010" when oaddr = "000100" else
+		 "0000001" when oaddr = "000101" else
+		 "0000000" when oaddr = "000110" else
+		 "0000011" when oaddr = "000111" else
+		 "0011011" when oaddr = "001000" else
+   		 "0011000" when oaddr = "001001" else
+		 "0011001" when oaddr = "001010" else
+		 "0011010" when oaddr = "001011" else
+		 "0010010" when oaddr = "001100" else
+		 "0010001" when oaddr = "001101" else
+		 "0010000" when oaddr = "001110" else
+		 "0010011" when oaddr = "001111" else
+		 "0000000" when oaddr = "010000" else
+		 "0000000" when oaddr = "010001" else
+		 "0000101" when oaddr = "010010" else
+		 "0000100" when oaddr = "010011" else
+		 "0100010" when oaddr = "010100" else
+		 "0100001" when oaddr = "010101" else
+		 "0100000" when oaddr = "010110" else
+		 "0100011" when oaddr = "010111" else
+		 "0000000" when oaddr = "011000" else
+		 "0000000" when oaddr = "011001" else
+		 "0001101" when oaddr = "011010" else
+		 "0001100" when oaddr = "011011" else
+		 "0101010" when oaddr = "011100" else
+		 "0101001" when oaddr = "011101" else
+		 "0101000" when oaddr = "011110" else
+		 "0101011" when oaddr = "011111" else
+		 "0111011" when oaddr = "100000" else
+   		 "0111000" when oaddr = "100001" else
+		 "0111001" when oaddr = "100010" else
+		 "0111010" when oaddr = "100011" else
+		 "0110010" when oaddr = "100100" else
+		 "0110001" when oaddr = "100101" else
+		 "0110000" when oaddr = "100110" else
+		 "0110011" when oaddr = "100111" else
+		 "1001011" when oaddr = "101000" else
+   		 "1001000" when oaddr = "101001" else
+		 "1001001" when oaddr = "101010" else
+		 "1001010" when oaddr = "101011" else
+		 "1000010" when oaddr = "101100" else
+		 "1000001" when oaddr = "101101" else
+		 "1000000" when oaddr = "101110" else
+		 "1000011" when oaddr = "101111" else
+		 "0000000"; 
 
    clock: process(CLK, RESET) is 
    begin
@@ -122,12 +202,15 @@ begin
 		   zero <= '0';
 		end if;
 
-		--outsel counter
-		if  cs = none then
-		   osel <= (others => '0');
-		elsif cs = incsel then
-		   osel <= osel + 1;
-		end if;
+		if cs = none then 
+			oaddr <= (others => '0');
+		else
+			if cs = incsel then
+				oaddr <= oaddr + 1; 
+			end if; 
+		end if; 
+
+		
 
 		--output latching
 		SOUT <= lsout;
@@ -142,7 +225,7 @@ begin
 
 
    -- FSM for output
-   fsm: process(cs, gset, iset, pgareset, fset, osel) is
+   fsm: process(cs, gset, iset, pgareset, fset, oaddr) is
    begin
    	case cs is
 		when none =>
@@ -150,11 +233,11 @@ begin
 			latch <= '0';
 			chansel <= '0'; 
 			if gset = '1' or iset = '1' or fset = '1' or pgareset = '1' then
-			   ns <= write_wait;
+			   ns <= writew;
 			else
 			   ns <= none;
  			end if;
-		when write_wait => 
+		when writew => 
 			lsclk <= '0';
 			latch <= '0';
 			chansel <= '0';
@@ -188,7 +271,7 @@ begin
 			lsclk <= '0';
 			latch <= '0';
 			chansel <= '1';
-			if osel = "1001111" then
+			if oaddr = "101111" then
 				ns <= latchl;
 			else 
 				ns <= clkl1;
@@ -215,28 +298,5 @@ begin
 			ns <= none;
 	end case;
    end process; 
-
-   --- combinational stuff
-   g <= "000" when zero = '1' else GAIN;
-   i <= "00" when zero = '1' else ISEL;
-   f <= "00" when zero = '1' else FILTER;
-
-   gwe <= GSET or zero;
-   fwe <= FSET or zero;
-   iwe <= ISET or zero;
-
-   lsout <= bitout when zero ='0' else '0';
-   bitout <= lbit(0) when osel(2 downto 0) = "000" else
-   		   lbit(1) when osel(2 downto 0) = "001" else
-		   lbit(2) when osel(2 downto 0) = "010" else
-		   lbit(3) when osel(2 downto 0) = "011" else
-		   lbit(4) when osel(2 downto 0) = "100" else
-		   lbit(5) when osel(2 downto 0) = "101" else
-		   lbit(6) when osel(2 downto 0) = "110" else
-		   '0';
-  
-  addr <= osel(6 downto 3) when chansel = '1' else CHAN;
-
-  
 
 end Behavioral; 

9c12746d899b8dc793d35651946ffb00e4bce860
jonas@soma.mit.edu <jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2005-09-25 22:46:00 +0000

    * great results with hpf cap
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/acqboard/trunk@1137 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/benchmarks/raw/Experiments.py
+++ tests/benchmarks/raw/Experiments.py
@@ -122,13 +122,14 @@ def simpleTest(filename):
                100:7}
     b.gainSet = gainSet
     b.hpfs = [0, 1]
-    b.gains = [1, 2, 5, 10, 20, 50, 100]
-    f1 = 50
-    f2 = 10000
-    s.freqs = logspace(log10(f1), log10(f2), 50.)
-    #s.freqs = linspace(f1, f2, 20)
+    #b.gains = [1, 2, 5, 10, 20, 50, 100]
+    b.gains = [1]
+    f1 = 20
+    f2 = 1000
+    #s.freqs = logspace(log10(f1), log10(f2), 100.)
+    s.freqs = linspace(f1, f2, 100)
     
-    s.vpps = [3.6]
+    s.vpps = [4.05]
     
     e.A4.append((b, s))
     #b2 = boardstates.BoardStates()

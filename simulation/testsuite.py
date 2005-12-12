cdab61e0a5fbefe29eb807b6626753325b4fe17f
jonas@soma.mit.edu <jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2005-12-12 13:07:16 +0000

    * all tests pass, still need to add filter preprocessing
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/acqboard/trunk@1207 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- simulation/testsuite.py
+++ simulation/testsuite.py
@@ -17,7 +17,7 @@ else:
     suite.addTest(vhdltest.VhdlSimTestCase("acqcmd"))
     suite.addTest(vhdltest.VhdlSimTestCase("fibertx"))
 
-    #suite.addTest(vhdltest.VhdlSimTestCase("filter"))
+    suite.addTest(vhdltest.VhdlSimTestCase("filter"))
     # we need to do some post-processing for filter
     
     suite.addTest(vhdltest.VhdlSimTestCase("input"))
